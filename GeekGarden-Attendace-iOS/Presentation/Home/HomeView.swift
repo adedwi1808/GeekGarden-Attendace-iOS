8025dae59a246495c5426745538dda771416e7ac
adedwi1808 <adedwip1808@gmail.com>
2022-12-15 03:30:21 +0700

    ADD: Menambahkan HomeView

---
+++ GeekGarden-Attendace-iOS/Presentation/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GeekGarden-Attendace-iOS
+//
+//  Created by Ade Dwi Prayitno on 15/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
