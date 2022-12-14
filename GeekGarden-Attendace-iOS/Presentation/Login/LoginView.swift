691c5ed4966e72fbb7cc34bc1fe4e7c125a5e65a
adedwi1808 <adedwip1808@gmail.com>
2022-12-15 02:03:51 +0700

    FEAT: membuat tampilan login

--- GeekGarden-Attendace-iOS/Presentation/Login/LoginView.swift
+++ GeekGarden-Attendace-iOS/Presentation/Login/LoginView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         ZStack {
             Color("BackgroundColor")
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 10) {
+                Image("LoginVector")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth:  .infinity)
                 
+                Text("Login")
+                    .font(.system(size: 45, weight: .black))
+                    .frame(maxWidth: .infinity ,alignment: .leading)
+                    .foregroundColor(Color("PrimaryColor"))
+                
+                HStack {
+                    Image(systemName: "envelope")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color("PrimaryColor"))
+                    VStack {
+                        TextField("Email", text: $email)
+                        Color("PrimaryColor")
+                            .frame(height: 2)
+                    }
+                }
+                
+                HStack {
+                    Image(systemName: "lock")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color("PrimaryColor"))
+                    VStack {
+                        TextField("Password", text: $password)
+                        Color("PrimaryColor")
+                            .frame(height: 2)
+                    }
+                }
+                HStack {
+                    Text("Lupa Password ?")
+                        .foregroundColor(Color("PrimaryColor"))
+                    
+                    Text("Klik Disini")
+                        .foregroundColor(.blue)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    //
+                } label: {
+                    Text("Login")
+                        .font(.system(size: 22, weight: .bold))
+                        .padding(.all)
+                        .frame(maxWidth: .infinity)
+                }
+                .foregroundColor(.white)
+                .background(.green)
+                .cornerRadius(15)
+
             }
+            .padding(15)
         }
     }
 }
