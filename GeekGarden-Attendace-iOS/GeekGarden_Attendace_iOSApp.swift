9cbab04d72093c4ec4d08c153b22b32fcd8dd5a1
adedwi1808 <adedwip1808@gmail.com>
2022-12-15 00:41:56 +0700

    Initial Commit

---
+++ GeekGarden-Attendace-iOS/GeekGarden_Attendace_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeekGarden_Attendace_iOSApp.swift
+//  GeekGarden-Attendace-iOS
+//
+//  Created by Ade Dwi Prayitno on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct GeekGarden_Attendace_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
