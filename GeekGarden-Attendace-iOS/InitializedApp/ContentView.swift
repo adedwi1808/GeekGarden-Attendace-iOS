f55365592ca7e266f7dadd535e01a7a3925d161f
adedwi1808 <adedwip1808@gmail.com>
2022-12-15 01:19:45 +0700

    ADD: menambahkan background color

--- GeekGarden-Attendace-iOS/InitializedApp/ContentView.swift
+++ GeekGarden-Attendace-iOS/InitializedApp/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isLoggedIn: Bool = true
+    @State var isLoggedIn: Bool = false
     var body: some View {
         NavigationView {
             Group {
