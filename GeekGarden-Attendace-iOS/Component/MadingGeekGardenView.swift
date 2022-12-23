73fc216cd3dd31c87932d6dd4feff1c6095b6482
adedwi1808 <adedwip1808@gmail.com>
2022-12-24 00:04:38 +0700

    FIX: Memperbaiki struktur mading yang tidak simetris

--- GeekGarden-Attendace-iOS/Component/MadingGeekGardenView.swift
+++ GeekGarden-Attendace-iOS/Component/MadingGeekGardenView.swift
@@ -13,7 +13,7 @@ struct MadingGeekGardenView: View {
     let fotoMading: String
     let tanggalMading: String
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
+        VStack(alignment: .leading, spacing: 10) {
             AsyncImage(url: URL(string: "https://2d6f-182-253-183-13.ap.ngrok.io/storage/mading/\(fotoMading)")) { image in
                 image
                     .resizable()
@@ -31,20 +31,23 @@ struct MadingGeekGardenView: View {
             Text(judulMading)
                 .font(.system(size: 18))
                 .fontWeight(.semibold)
-                .frame(maxWidth: .infinity, alignment: .leading)
+                .lineLimit(1)
+                .frame(height: 25, alignment: .leading)
             Text(isiMading)
                 .lineLimit(3)
                 .frame(height: 70, alignment: .topLeading)
             
+            Spacer()
             HStack {
                 Image(systemName: "clock")
                 Text(tanggalMading)
+                    .font(.system(size: 14))
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .foregroundColor(.gray)
         }
         .frame(width: 300, height: 290)
-        .padding(10)
+        .padding(15)
         .background(.white)
         .cornerRadius(15)
         .shadow(radius: 2, x: 5, y: 3)
@@ -54,6 +57,6 @@ struct MadingGeekGardenView: View {
 
 struct MadingGeekGardenView_Previews: PreviewProvider {
     static var previews: some View {
-        MadingGeekGardenView(judulMading: "Judul Mading", isiMading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", fotoMading: "", tanggalMading: "")
+        MadingGeekGardenView(judulMading: "Judul Mading", isiMading: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", fotoMading: "", tanggalMading: "asdasda")
     }
 }
