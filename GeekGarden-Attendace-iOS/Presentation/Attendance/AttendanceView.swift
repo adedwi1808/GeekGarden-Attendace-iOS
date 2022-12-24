7403c3cff81a2ee974c5fd5dbb6236da71a045a0
adedwi1808 <adedwip1808@gmail.com>
2022-12-24 18:49:01 +0700

    ADD: AttendanceView & ViewModel Logic

--- GeekGarden-Attendace-iOS/Presentation/Attendance/AttendanceView.swift
+++ GeekGarden-Attendace-iOS/Presentation/Attendance/AttendanceView.swift
@@ -8,8 +8,83 @@
 import SwiftUI
 
 struct AttendanceView: View {
+    @StateObject var attendanceViewModel: AttendanceViewModel = AttendanceViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
+            ScrollView {
+                Text(attendanceViewModel.timeString(date: attendanceViewModel.date))
+                    .font(.system(size: 72, weight: .semibold))
+                    .foregroundColor(Color("PrimaryColor"))
+                Text(attendanceViewModel.dateString(date: attendanceViewModel.date))
+                    .font(.system(size: 24, weight: .regular))
+                    .foregroundColor(Color("PrimaryColor"))
+                
+                NavigationLink(destination: CheckInView()) {
+                    ZStack {
+                        Circle()
+                            .foregroundColor(.green)
+                            .shadow(radius: 5)
+                            .padding(5)
+                        Image("touchSymbol")
+                            .resizable()
+                            .scaledToFit()
+                            .scaleEffect(0.6)
+                    }
+                    .frame(height: 300)
+                    .padding(.vertical, 20)
+                }
+                
+                
+                HStack {
+                    Image(systemName: "mappin")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.red)
+                        .frame(height: 24)
+                    Text("GeekGarden Office")
+                        .font(.system(size: 24, weight: .semibold))
+                        .foregroundColor(Color("PrimaryColor"))
+                }
+                .padding(.bottom, 20)
+                
+                HStack(alignment: .center, spacing: 60) {
+                    VStack {
+                        Image("CheckInSymbol")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                        Text("22.0")
+                    }
+                    
+                    VStack {
+                        Image("WorkIntervalSymbol")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                        Text("22.0")
+                    }
+                    
+                    VStack {
+                        Image("CheckOutSymbol")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 40)
+                        Text("22.0")
+                    }
+                }
+                .font(.system(size: 24))
+                .foregroundColor(Color("PrimaryColor"))
+                .padding(.top, 20)
+            }
+        }
+        .onAppear {
+            attendanceViewModel.updateTimer
+        }
+
     }
 }
 
--- GeekGarden-Attendace-iOS/Presentation/Attendance/AttendanceViewModel.swift
+++ GeekGarden-Attendace-iOS/Presentation/Attendance/AttendanceViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class AttendanceViewModel: ObservableObject {
+    @Published var date: Date = Date()
+    
+    private var timeFormat: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh.mm"
+        return formatter
+    }
+    
+    private var dateFormat: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, dd MMMM yyyy"
+        return formatter
+    }
+    
+    var updateTimer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {_ in 
+            self.date = Date()
+        })
+    }
+    
+    func timeString(date: Date) -> String {
+        timeFormat.string(from: date)
+    }
+    
+    func dateString(date: Date) -> String {
+        dateFormat.string(from: date)
+    }
+    
+}
