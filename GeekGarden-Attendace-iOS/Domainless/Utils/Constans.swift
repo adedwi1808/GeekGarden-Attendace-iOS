ffea6d74ab1b694281d55edbcdf5bef0abbe7254
adedwi1808 <adedwip1808@gmail.com>
2023-01-03 19:41:21 +0700

    ADD: Utils Constants untuk menyimpan base URL

---
+++ GeekGarden-Attendace-iOS/Domainless/Utils/Constans.swift
@@ -0,0 +1,12 @@
+//
+//  Constans.swift
+//  GeekGarden-Attendace-iOS
+//
+//  Created by Ade Dwi Prayitno on 03/01/23.
+//
+
+import Foundation
+
+class Constans {
+    let baseURL: String = "5ff0-182-253-183-4.ap.ngrok.io"
+}
